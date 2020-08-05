5e93c2260754db2261ebb1af8f849e08ea9b4171
Jonathan Samson <jonathan.samson@wur.nl>
2020-08-05 07:32:41 +0200

    Initial commit

---
+++ decoder.d
@@ -0,0 +1,34 @@
+/*
+ * Takes a base64 binary and converts it to readable text.
+ *
+ * called with:
+ *	cat filename.txt | base64 -d | ./decoder
+ *
+ * Base64.decode("BASE64STRINGHERE");
+ * -> Needs to get into fread-style
+ */
+import core.stdc.stdio: fread, stdin;
+import std.stdio;
+import std.bitmanip;
+import std.conv;
+import std.base64;
+
+void main()
+
+{
+	float f;
+	int i = 1;
+	string output = "";
+	while (fread(&f, 1, 4, stdin) == 4)
+	{
+		ubyte[4] swappedF = (nativeToLittleEndian(f));
+		output ~= bigEndianToNative!float(swappedF).to!string ~ "\t";
+		if(i%2 == 0)
+		{
+			writeln(output);
+			output = "";
+		}
+		//writeln(bigEndianToNative!float(swappedF));
+		++i;
+	}
+}
