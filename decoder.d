d456adb071a663184a034d495b1c4d20a5dc74ed
Jonathan Samson <jonathan.samson@wur.nl>
2020-08-05 07:54:42 +0200

    Enabled usage for already-read-in string.

--- decoder.d
+++ decoder.d
@@ -2,13 +2,8 @@
  * 
  * Author: Jonathan Samson
  * Date: 04-08-2020
- *
- * called with:
- *	cat filename.txt | base64 -d | ./decoder
- *
- * Base64.decode("BASE64STRINGHERE");
- * -> Needs to get into fread-style
  */
+module mzxmldecoder;
 import core.stdc.stdio: fread, stdin;
 import std.stdio;
 import std.bitmanip;
@@ -16,8 +11,25 @@ import std.conv;
 import std.base64;
 
 void main()
-
 {
+	string line = "Qk3fDkS2lldCl6euRND28UKXvA9EyKoPQsg6lES2z/hCyxPbRLpQkELXhZBE4gXdQuzjCUTKe4VDAhZoRxKMVUMS9gVE0dn6QysnFUTcG4NDRy59Rwo27ENzK95H0YRqQ3Qsd0YINMA=";
+	ubyte[] decoded = Base64.decode(line);
+	string output = "";
+	int j;
+	for(int i=1; i<=decoded.length/4; ++i)
+	{
+		ubyte[4] swappedF = decoded[(4*i)-4..(4*i)];
+		float real_text = bigEndianToNative!float(swappedF);
+		output ~= real_text.to!string ~ "\t";
+		if(i%2 == 0)
+		{
+			writeln(output);
+			output = "";
+		}
+		j = i;
+	}
+	writeln(j/2);
+	/*
 	float f;
 	int i = 1;
 	string output = "";
@@ -32,4 +44,5 @@ void main()
 		}
 		++i;
 	}
+	*/
 }
